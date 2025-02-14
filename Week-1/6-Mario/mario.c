16fe936b04587d9d0f6491cc0c2c6123566ac3ce
Marcel Marciniak <marciniak.marc@gmail.com>
2025-02-14 19:08:14 +0100

    mario-more problem completed

--- Week-1/6-Mario/mario.c
+++ Week-1/6-Mario/mario.c
@@ -4,13 +4,29 @@ int main(void){
 
     int size;
 
-    do{
+    // Keep asking for positive non-zero integer
+    while (1) {
         printf("Size: ");
-        scanf("%d", &size);
-    } while (size < 1);
+        if (scanf("%d", &size) == 1 && size > 0) {
+            break;
+        } else {
+            // getchar() reads input field one character at a time
+            // and discards them until it reaches \n (which is left by scanf)
+            // at this point the loop restarts again from the Size: prompt
+            while (getchar() != '\n');
+            printf("Invalid input. Please enter a positive integer.\n");
+        }
+    }
 
     for (int i = 0; i < size; i++){
-        for (int j = 0; j < size; j++){
+        for (int k = size - i; k > 1; k--){
+            printf(" ");
+        }
+        for (int j = -1; j < i; j++){
+            printf("#");
+        }
+        printf("  ");
+        for (int j = -1; j < i; j++){
             printf("#");
         }
         printf("\n");
